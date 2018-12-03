e89d77e42e4e4595bf16c6ba73391a106fd1589d
Michelle Cheng <mcheng24@illinois.edu>
2018-12-03 16:30:07 -0600

    dynamic programming implementation

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,46 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /**
+         * Valid inputs
+         */
+        Ncoin obj = new Ncoin(2,3,10);
+        obj.makeChange();
+        //obj.getCoins();
+        System.out.println(obj);
+        /*Ncoin obj2 = new Ncoin(5,7,10);
+        boolean change2 = obj2.makeChange();
+        System.out.println(change2);
+        if (change2) {
+            System.out.println("How many of the first coin needed: " + obj2.a1 + "\nHow many of the first coin needed: " + obj2.a2);
+        }
+        Ncoin obj3 = new Ncoin(1,3,0);
+        boolean change3 = obj3.makeChange();
+        System.out.println(change3);
+        if (change3) {
+            System.out.println("How many of the first coin needed: " + obj3.a1 + "\nHow many of the first coin needed: " + obj3.a2);
+        }
+        Ncoin obj4 = new Ncoin(8,7,100);
+        boolean change4 = obj4.makeChange();
+        System.out.println(change4);
+        if (change4) {
+            System.out.println("How many of the first coin needed: " + obj4.a1 + "\nHow many of the first coin needed: " + obj4.a2);
+        }*/
+        /**
+         * Invalid inputs
+         */
+        /*Ncoin obj5 = new Ncoin(5,7,-4);
+        boolean change5 = obj5.makeChange();
+        System.out.println(change5);
+        if (change5) {
+            System.out.println("How many of the first coin needed: " + obj5.a1 + "\nHow many of the first coin needed: " + obj5.a2);
+        }
+        Ncoin obj6 = new Ncoin(-5,7,8);
+        boolean change6 = obj6.makeChange();
+        System.out.println(change6);
+        if (change6) {
+            System.out.println("How many of the first coin needed: " + obj6.a1 + "\nHow many of the first coin needed: " + obj6.a2);
+        }
         Scanner read = new Scanner(System.in);
         System.out.println("Enter a value for coin 1: ");
         int x1 = read.nextInt();
@@ -11,11 +51,11 @@ public class Main {
         int n = read.nextInt();
         System.out.println("First coin value: " + x1 + "\nSecond coin value: " + x2 + "\nN value: " + n);
 
-        Ncoin obj = new Ncoin(x1, x2, n);
-        boolean change = obj.makeChange();
-        System.out.println(change);
-        if (change) {
-            System.out.println("How many of the first coin needed: " + obj.a1 + "\nHow many of the first coin needed: " + obj.a2);
-        }
+        Ncoin obj7 = new Ncoin(x1, x2, n);
+        boolean change7 = obj7.makeChange();
+        System.out.println(change7);
+        if (change7) {
+            System.out.println("How many of the first coin needed: " + obj7.a1 + "\nHow many of the first coin needed: " + obj7.a2);
+        }*/
     }
 }
--- src/Ncoin.java
+++ src/Ncoin.java
@@ -1,50 +1,78 @@
+import java.util.Arrays;
+
 public class Ncoin {
     /**
-     * x1 is the value of the first coin.
-     * x2 is the value of the second coin.
+     * x1 value of first coin.
+     * x2 value of second coin.
+     * c stores the amount needed of each coin.
      * n is the N value that the coins could make change of.
-     * a1 is the amount of first coins you need to make N.
-     * a2 is the amount of second coins you need to make N.
+     * cAmount is the amount of coins needed of each to make n.
+     * CHENG is a boolean array that determines whether the first and second coin add up to n.
      */
     private int x1;
     private int x2;
     private int n;
-    public int a1;
-    public int a2;
-    /**
-     * c1 is the value of the first coin.
-     * c2 is the value of the second coin.
+    private int[][] c;
+    private boolean[] CHENG;
+     /**
+     * x1 is the value of the first coin.
+     * x2 is the value of the second coin.
      */
-    public Ncoin(final int c1, final int c2, final int N) {
-        x1 = c1;
-        x2 = c2;
+    public Ncoin(final int coin1, final int coin2, final int N) {
+        x1 = coin1;
+        x2 = coin2;
         n = N;
+        CHENG = new boolean[n + 1];
+        c = new int[n+1][2];
     }
 
     /**
      * @return if a combination of coins add up to a value N
      */
-    public boolean makeChange() {
-        if (n == 0) {
-            return true;
+    public boolean[] makeChange() {
+        if (n < 0 || x1 <= 0 || x2 <= 0 || x1 > n || x2 > n) {
+            return CHENG;
         }
-        int[][] CHENG = new int[n][n];
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                CHENG[i][j] = x1 * i + x2 * j;
-                System.out.print(CHENG[i][j] + " ");
-            }
-            System.out.println();
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                if (CHENG[i][j] == n) {
-                    a1 = i;
-                    a2 = j;
-                    return true;
+        CHENG[0] = true;
+        for (int i = 1; i <= n; i++) {
+            if (i>= x1 || i >= x2) {
+                CHENG[i] = CHENG[i - x1] || CHENG[i - x2];
+                if (CHENG[i] && CHENG[i] == CHENG[i - x1]) {
+                    c[i][0]++;
+                } else if (CHENG[i] && CHENG[i] == CHENG[i - x2]) {
+                    c[i][1]++;
                 }
             }
         }
-        return false;
+        return CHENG;
+    }
+    /**
+     * assigns coin combinations to elements in CHENG.
+     */
+    public int[][] getCoins() {
+        if (n < 0 || x1 <= 0 || x2 <= 0 || x1 > n || x2 > n) {
+            System.out.println("Invalid Input");
+            return null;
+        } else {
+            for (int i = 0; i <= n; i++) {
+                if (CHENG[i]) {
+                    for (int j = 0; j < i; j++) {
+                        for (int k = 0; k < i; k++) {
+                            if (n == j*x1 + k*x2) {
+                                c[i][0] = j;
+                                c[i][1] = k;
+                            }
+                        }
+                    }
+                }
+            }
+            return c;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(CHENG) +
+                "\n" + Arrays.deepToString(c);
     }
 }
